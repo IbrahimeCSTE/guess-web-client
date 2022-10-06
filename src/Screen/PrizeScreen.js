c823f38fe49ed0d1291fc0f3961e8e664c1e079d
cste14 <emonnstu14@gmail.com>
2022-10-07 01:20:44 +0600

    testlast

--- src/Screen/PrizeScreen.js
+++ src/Screen/PrizeScreen.js
@@ -8,13 +8,17 @@ const PrizeScreen = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const winnerPrizeDetails = await axios.get("/api/winner-details");
+      const winnerPrizeDetails = await axios.get(
+        "https://server.kajitbe.com/api/winner-details"
+      );
       setWinnerPrize(winnerPrizeDetails.data);
-      const { data } = await axios.get("/api/update-news");
+      const { data } = await axios.get(
+        "https://server.kajitbe.com/api/update-news"
+      );
       setuNews(data[data.length - 1]);
-      const res1 = await axios.get("/api/fb-link");
+      const res1 = await axios.get("https://server.kajitbe.com/api/fb-link");
       setfLink(res1.data[data.length - 1]);
-      const res = await axios.get("/api/yt-link");
+      const res = await axios.get("https://server.kajitbe.com/api/yt-link");
       setyLink(res.data[data.length - 1]);
     };
     fetchData();
