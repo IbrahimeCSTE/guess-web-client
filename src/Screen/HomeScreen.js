241a7649a8f0eea80f99e79709542dbc424ac17f
cste14 <emonnstu14@gmail.com>
2022-10-16 02:54:42 +0600

    fix firebase1

--- src/Screen/HomeScreen.js
+++ src/Screen/HomeScreen.js
@@ -3,11 +3,14 @@ import Header from "../Component/Header1/Header";
 import DynamicText from "./DynamicText";
 import FooterScreen from "../Screen/FooterScreen";
 import Sports from "./Sports";
+
 import { getAnalytics, logEvent } from "firebase/analytics";
+
 import { useEffect } from "react";
+import app from "../Component/Firebase/FirebaseAuth";
 
 const HomeScreen = () => {
-  const googleAnalytics = getAnalytics();
+  const googleAnalytics = getAnalytics(app);
 
   useEffect(() => {
     logEvent(googleAnalytics, "visited all client");
